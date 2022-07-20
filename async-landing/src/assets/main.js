88090caf8fb8124e31c6f712f76496ed9ffc5ab7
GGGamesXDlol <63313393+GGGamesXDlol@users.noreply.github.com>
2022-07-20 12:31:58 +0200

    Update main.js

--- async-landing/src/assets/main.js
+++ async-landing/src/assets/main.js
@@ -29,7 +29,7 @@ async function fetchData(urlApi)
             <div class="group relative">
                 <div
                 class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                    <img src="${video.video.thumbnails[0].url}" alt="${video.video.title}" class="w-full">
+                    <img src="${video.video.thumbnails[4].url}" alt="${video.video.title}" class="w-full">
                 </div>
                 <div class="mt-4 flex justify-between">
                     <h3 class="text-sm text-gray-700">
